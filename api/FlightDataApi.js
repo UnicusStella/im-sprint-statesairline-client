f7c04ef9ba4c9e4bb1a5ad4623059cf47d518bdc
UnicusStella <unicusstella@gmail.com>
2021-08-23 00:39:59 +0900

    FlightDataApi Complete

--- api/FlightDataApi.js
+++ api/FlightDataApi.js
@@ -8,27 +8,13 @@ if (typeof window !== 'undefined') {
 export function getFlight(filterBy = {}) {
   // HINT: 가장 마지막 테스트를 통과하기 위해, fetch를 이용합니다. 아래 구현은 완전히 삭제되어도 상관없습니다.
   // TODO: 아래 구현을 REST API 호출로 대체하세요.
+  let query = '';
+  if (filterBy.departure) query = query + `departure=${filterBy.departure}&`;
 
-  let json = [];
-  if (typeof window !== 'undefined') {
-    json = localStorage.getItem('flight');
-  }
-  const flight = JSON.parse(json) || [];
+  if (filterBy.destination)
+    query = query + `destination=${filterBy.destination}`;
 
-  return new Promise((resolve) => {
-    const filtered = flight.filter((flight) => {
-      let condition = true;
-      if (filterBy.departure) {
-        condition = condition && flight.departure === filterBy.departure;
-      }
-      if (filterBy.destination) {
-        condition = condition && flight.destination === filterBy.destination;
-      }
-      return condition;
-    });
+  let url = `http://ec2-13-124-90-231.ap-northeast-2.compute.amazonaws.com:81/flight?${query}`;
 
-    setTimeout(() => {
-      resolve(filtered);
-    }, 500);
-  });
+  return fetch(url).then((response) => response.json());
 }
